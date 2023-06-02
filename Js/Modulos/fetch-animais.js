14803ef64d52bceeec9097abe35c932d076013a9
Júlio César <juliowanke@gmail.com>
2023-06-01 21:24:19 -0300

    anima-numeros-refatorado

--- Js/Modulos/fetch-animais.js
+++ Js/Modulos/fetch-animais.js
@@ -1,4 +1,4 @@
-import initAnimaNumeros from "./animaNumeros.js";
+import AnimaNumeros from "./animaNumeros.js";
 
 export default function InitFetchAnimais() {
   function createAnimal(animal) {
@@ -16,7 +16,8 @@ export default function InitFetchAnimais() {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
-      initAnimaNumeros();
+      const animaNumeros = new AnimaNumeros("[data-numero]", ".numeros", "ativo");
+      animaNumeros.init();
     } catch (erro) {
       console.log(Error(erro));
     }
