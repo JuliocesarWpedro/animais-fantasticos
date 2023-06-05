e91f078ec84cc726555c2deefc544f80aebf9aac
Júlio César <juliowanke@gmail.com>
2023-06-05 19:57:27 -0300

    mudança no outsideClick

--- Js/Modulos/outsideClick.js
+++ Js/Modulos/outsideClick.js
@@ -1,6 +1,16 @@
 export default function outsideClick(element, events, callback) {
   const html = document.documentElement;
   const outside = "data-outside";
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      events.forEach((UserEvent) => {
+        html.removeEventListener(UserEvent, handleOutsideClick);
+      });
+      callback();
+    }
+  }
+
   if (!element.hasAttribute(outside)) {
     events.forEach((UserEvent) => {
       setTimeout(() => {
@@ -8,14 +18,5 @@ export default function outsideClick(element, events, callback) {
       });
     });
     element.setAttribute(outside, "");
-  }
-  function handleOutsideClick(event) {
-    if (!element.contains(event.target)) {
-      element.removeAttribute(outside, "");
-      events.forEach((UserEvent) => {
-        html.removeEventListener(UserEvent, handleOutsideClick);
-      });
-      callback();
-    }
   }
 }
